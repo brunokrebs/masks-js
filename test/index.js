c7e4451dbc5f169fdd77ca6f42576f366cf13e87
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-17 00:28:13 -0200

    100% test coverage and first release

--- test/index.js
+++ test/index.js
@@ -2,6 +2,8 @@ import * as assert from 'assert';
 import maskUSPhone from '../src/index';
 
 const testSamples = [
+  { input: null, expectedResult: null, description: 'should return null when null is passed' },
+  { input: undefined, expectedResult: null, description: 'should return null when undefined is passed' },
   { input: 'abc', expectedResult: 'abc', description: 'should return pristine value when receiving "abc"' },
   { input: 'abc1234567', expectedResult: 'abc1234567', description: 'should return pristine value when receiving "abc1234567"' },
   { input: 'abcdefghij', expectedResult: 'abcdefghij', description: 'should return pristine value when receiving "abcdefghij"' },
